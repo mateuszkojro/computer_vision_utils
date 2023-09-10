1531caeed918fd17ae7db0c4db5bae5cee28d2e6
Mateusz Kojro <mateuszkojro@outlook.com>
2023-09-10 20:21:33 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 metadata = dict(
     name="computer_vision_utils",
-    version="0.1.0",
+    version="0.1.1",
     packages=["computer_vision_utils"],
     install_requires=["numpy", "opencv-python", "matplotlib"],
 )
